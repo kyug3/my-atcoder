dc9d3508b356247622715b4d8d84bfd464991e56
Eguy <atyuge@gmail.com>
2021-01-06 03:07:48 +0900

    update

--- JOI/Silk_roal.py
+++ JOI/Silk_roal.py
@@ -5,7 +5,13 @@ weather = [int(input()) for _ in range(M)]
 dp = [[float('inf')] * (M + 1) if n else [0] * (M + 1) for n in range(N + 1)]
 
 for i in range(1, N + 1):
+    mins = [float('inf')] * (i + 1)
     for j in range(i, M + 1):
-        dp[i][j] = min(dp[i-1][:j]) + dist[i-1] * weather[j-1]
+        if i == 1:
+            dp[i][j] = dist[i-1] * weather[j-1]
+        else:
+            dp[i][j] = last_mins[j] + dist[i-1] * weather[j-1]
+        mins.append(min(mins[-1], dp[i][j]))
+    last_mins = mins.copy()
 
 print(min(dp[-1]))
