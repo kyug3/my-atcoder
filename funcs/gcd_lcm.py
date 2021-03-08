fddac98fe29ab5bb51648a34ef238fb9af474183
Eguy <atyuge@gmail.com>
2021-03-08 15:45:19 +0900

    update

--- funcs/gcd_lcm.py
+++ funcs/gcd_lcm.py
@@ -1,16 +1,15 @@
 import math
-from typing import List
 from functools import reduce
 
 
-def gcd(nums: List[int]) -> int:
+def gcd(nums) -> int:
     return reduce(math.gcd, nums)
     
 def lcm_base(a: int, b: int) -> int:
     return (a * b) // math.gcd(a, b)
 
-def lcm(nums: List[int]) -> int:
-    return reduce(lcm_base, numbers, 1)
+def lcm(nums) -> int:
+    return reduce(lcm_base, nums, 1)
 
 def ext_gcd(a, b):
     if b == 0:
@@ -22,15 +21,3 @@ def ext_gcd(a, b):
 
 # greatest common divisor
 # least common multiple
-
-
-def my_gcd(a: int, b: int) -> int:
-    while b > 0:
-        a, b = b, a % b
-    return b
-
-def my_gcd2(a, b):
-    if b == 0:
-        return a
-    else:
-        return my_gcd2(b, a % b)
