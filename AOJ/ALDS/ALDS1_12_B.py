011706339aacf053f3105ab6046b9e9050bd5127
Eguy <atyuge@gmail.com>
2021-04-11 13:04:44 +0900

    update

--- AOJ/ALDS/ALDS1_12_B.py
+++ AOJ/ALDS/ALDS1_12_B.py
@@ -1,5 +1,14 @@
-import heapq
+N = int(input())
+graph = [[] for _ in range(N)]
+for _ in range(N):
+    lst = list(map(int, input().split()))
+    u, k = lst[0], lst[1]
+    lst = lst[2:]
+    for i in range(0, len(lst), 2):
+        v, c = lst[i], lst[i+1]
+        graph[u].append((c, v))
 
+import heapq
 
 def dijkstra(graph, num_node, start):
     hq = [(0, start)]
@@ -16,3 +25,7 @@ def dijkstra(graph, num_node, start):
                 distance[n] = now_dist
                 heapq.heappush(hq, (now_dist, n))
     return distance
+
+dists = dijkstra(graph, N, 0)
+for i in range(N):
+    print(i, dists[i])
