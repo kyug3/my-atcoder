452aae8a34eb855fcce58b8867472c77616dc4c2
Eguy <atyuge@gmail.com>
2021-05-27 14:49:36 +0900

    update

--- funcs/BIT.py
+++ funcs/BIT.py
@@ -3,14 +3,14 @@ A = list(map(int, input().split()))
 BIT = [0] * (N+1)
 
 def add(i, x):
-    # 1-index
+    # 1-indexed
     # i番目の要素にxを加える
     while i <= N:
         BIT[i] += x
         i += i & -i
 
 def out(i):
-    # 1-index
+    # 1-indexed
     # i番目の要素までの総和を返す
     ans = 0
     while i > 0:
