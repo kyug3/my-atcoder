5a4ee727b65e472b69682514b73e50ab999cca5e
Eguy <atyuge@gmail.com>
2021-05-27 15:12:48 +0900

    update

--- funcs/BIT.py
+++ funcs/BIT.py
@@ -1,19 +1,21 @@
+class BIT:
+    def __init__(self, n):
+        self.size = n
+        self.tree = [0] * (n + 1)
+  
+    def sum(self, i):
+        # i番目の要素までの総和を返す
+        s = 0
+        while i > 0:
+            s += self.tree[i]
+            i -= i & -i
+        return s
+  
+    def add(self, i, x):
+        # i番目の要素にxを足す
+        while i <= self.size:
+            self.tree[i] += x
+            i += i & -i
+
 N = int(input())
-A = list(map(int, input().split()))
-BIT = [0] * (N+1)
-
-def add(i, x):
-    # 1-indexed
-    # i番目の要素にxを加える
-    while i <= N:
-        BIT[i] += x
-        i += i & -i
-
-def out(i):
-    # 1-indexed
-    # i番目の要素までの総和を返す
-    ans = 0
-    while i > 0:
-        ans += BIT[i]
-        i -= i & -i
-    return ans
+bit = BIT(N)
