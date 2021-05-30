a833b20b3581bd2758b6daeb9a04dc636b99529a
Eguy <atyuge@gmail.com>
2021-05-30 14:32:29 +0900

    update

--- typical90/030.py
+++ typical90/030.py
@@ -5,29 +5,17 @@ mod = 10**9 + 7
 input = sys.stdin.readline
 def li(): return list(map(int, input().split()))
 
-def sieve_of_eratosthenes(n):
-    primes = []
-    is_prime = [True] * (n + 1)
-    is_prime[0] = False
-    is_prime[1] = False
-
-    for p in range (n + 1):
-        if not is_prime[p]:
-            continue
-        primes.append(p)
-        for i in range(p*p, n+1, p):
-            is_prime[i] = False
-
-    return primes, is_prime
-
-
 N, K = li()
 
-primes, is_prime = sieve_of_eratosthenes(N+1)
-count = [0] * (N+2)
-for i in primes:
+is_prime = [True] * (N+1)
+count = [0] * (N+1)
+
+for i in range(2, N+1):
+    if not is_prime[i]:
+        continue
     for j in range(i, N+1, i):
         count[j] += 1
+        is_prime[j] = False
 
 ans = 0
 for i in count:
