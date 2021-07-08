64f69dc283259863fd11f650277023957958168d
Eguy <atyuge@gmail.com>
2021-07-08 23:45:37 +0900

    update

---
+++ ABC/ABC075/A.py
@@ -0,0 +1,9 @@
+import sys, math
+sys.setrecursionlimit(10**6)
+mod = 10**9 + 7
+#mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
+
+A, B, C = li()
+print(A^B^C)
