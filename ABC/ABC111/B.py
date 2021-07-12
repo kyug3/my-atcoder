ef58aaf3a374cf61dede8fa0e1b3a150b24536ad
Eguy <atyuge@gmail.com>
2021-07-12 16:32:36 +0900

    update

---
+++ ABC/ABC111/B.py
@@ -0,0 +1,12 @@
+import sys, math
+sys.setrecursionlimit(10**6)
+mod = 10**9 + 7
+#mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
+
+N = int(input())
+for i in range(111, 1000, 111):
+    if N <= i:
+        print(i)
+        exit()
