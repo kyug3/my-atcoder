2184db315fc14a073ea7ce8ace1ef215b9dc714e
Eguy <atyuge@gmail.com>
2021-07-22 14:53:43 +0900

    update

---
+++ ABC/ABC041/B.py
@@ -0,0 +1,14 @@
+import sys, math
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+mod = 10**9 + 7
+#mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
+
+A, B, C = li()
+
+A *= B
+A %= mod
+A *= C
+print(A % mod)
