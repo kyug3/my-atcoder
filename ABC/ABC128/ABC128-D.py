a119401238f7d287cbdf8911f8a18f8e9c566f75
Eguy <atyuge@gmail.com>
2021-07-28 15:44:49 +0900

    update

--- ABC/ABC128/ABC128-D.py
+++ ABC/ABC128/ABC128-D.py
@@ -1,46 +1,28 @@
 import sys, math
-from collections import deque, defaultdict
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6)
+INF = float('inf')
 mod = 10**9 + 7
 #mod = 998244353
-input = sys.stdin.readline
-def g_inp(N, M):
-    g = [[] for _ in range(N)]
-    for _ in range(M):
-        a, b, z = map(int, input().split())
-        a -= 1; b -= 1
-        g[a].append((z, b))
-        g[b].append((z, a))
-    return g
-def l_inp(): return list(map(int, input().split()))
-def n_inp(): return map(int, input().split())
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
 
-
-N, K = n_inp()
-V = l_inp()
-rV = V[::-1]
-V2 = sorted(V)
-sv = sum(V)
+N, K = li()
+V = li()
 ans = 0
-for k in range(1, K+1):
-    x = 0
-    if k >= N:
-        x = sv - sum(V2[:K-k])
-        ans = max(x, ans)
-        continue
-    for i in range(k+1):
-        x = 0
-        lst = []
-        for j in range(i):
-            x += V[j]
-            if V[j] < 0:
-                lst.append(V[j])
-        for l in range(k-i):
-            x += rV[l]
-            if rV[l] < 0:
-                lst.append(rV[l])
-        lst.sort()
-        x -= sum(lst[:K-k])
-        ans = max(x, ans)
-
+for l in range(N+1):
+    for r in range(N+1):
+        if l + r > K or l + r > N:
+            break
+        lst = V[:l]
+        if r:
+            lst += V[-r:]
+        lst.sort(reverse=True)
+        for i in range(K-(l+r)):
+            if lst and lst[-1] < 0:
+                lst.pop()
+            else:
+                break
+        if lst:
+            ans = max(ans, sum(lst))
+        
 print(ans)
