65000452727a4f96af1cd15d34785f2a7847c6cf
Eguy <atyuge@gmail.com>
2021-08-08 02:03:39 +0900

    update

--- funcs/segtree/RAQ.py
+++ funcs/segtree/RAQ.py
@@ -1,8 +1,8 @@
-N = int(input())
 # N = 配列の長さ
 # data[N] が初期配列の 0 に対応する
 
-data = [0] * (2*N+1)
+#N = int(input())
+#data = [0] * (2*N+1)
 
 def update(l, r, v):
     # 区間[l, r) に vを足す
@@ -30,3 +30,16 @@ def query(x):
         ans += data[idx]
         #ans %= mod
     return ans
+
+
+# AOJ DSL_2_E
+
+N, q = map(int, input().split())
+data = [0] * (2*N+1)
+for _ in range(q):
+    tmp = list(map(int, input().split()))
+    if tmp[0] == 0:
+        l, r, v = tmp[1:]
+        update(l, r+1, v)
+    else:
+        print(query(tmp[1]))
