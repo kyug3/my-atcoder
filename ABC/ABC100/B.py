247cdfbfac90331eb22a382ca4f2878674813b5e
Eguy <atyuge@gmail.com>
2021-08-21 20:45:13 +0900

    update

---
+++ ABC/ABC100/B.py
@@ -0,0 +1,12 @@
+import sys, math
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+mod = 10**9 + 7
+#mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
+
+D, N = li()
+if N == 100:
+    N += 1
+print(100 ** D * N)
