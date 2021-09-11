99a1ac1f90f71e3d699209f82c344de6bdaa0b7d
Eguy <atyuge@gmail.com>
2021-09-12 01:15:00 +0900

    update

--- funcs/grid_processing.py
+++ funcs/grid_processing.py
@@ -1,9 +1,12 @@
-def rotate_right(A):
+def rotate_clock_wise(A):
     return list(zip(*(A[::-1])))
+
+def rotate_counter_clock_wise(A):
+    return list(zip(*(A)))[::-1]
     
 def trim_extra(A, rem='.'):
     for _ in range(4):
-        A = rotate_right(A)
+        A = rotate_clock_wise(A)
         while all(x == rem for x in A[-1]):
             A.pop()
     return A
