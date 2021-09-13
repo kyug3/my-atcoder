dc99a42a86d9eabb9388aa2b3aba04d90617393f
Eguy <atyuge@gmail.com>
2021-09-13 15:41:27 +0900

    update

---
+++ ABC/ABC085/ABC085-B.py
@@ -0,0 +1,11 @@
+import sys, math
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+mod = 10**9 + 7
+#mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
+
+N = int(input())
+d = set(int(input()) for _ in range(N))
+print(len(d))
