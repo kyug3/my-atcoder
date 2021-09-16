4cf7c875c2a2c4dd8982200ea34ecd9c081cacb9
Eguy <atyuge@gmail.com>
2021-09-17 01:34:17 +0900

    update

--- AGC/AGC018/A.py
+++ AGC/AGC018/A.py
@@ -1,19 +1,29 @@
 import sys, math
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6)
+INF = float('inf')
 mod = 10**9 + 7
 #mod = 998244353
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
 def li(): return list(map(int, input().split()))
-from functools import reduce
 
+"""
+あるAとB(A > B)について
+abs(A - B) = C
+abs(B - C) = D
+といった操作を繰り返すとユークリッドの互除法のようになり、
+最終的にAとBの最大公約数のボール G を入れられることがわかる。
+GはAの約数なのでAとGを用いてA以下の全てのGの倍数を入れられる。
+"""
 
-def gcd(nums) -> int:
-    return reduce(math.gcd, nums)
 N, K = li()
 A = li()
-m = max(A)
-g = gcd(A)
-if K % g == 0 and K <= m:
+maxA = max(A)
+
+gcd = A[0]
+for i in range(1, N):
+    gcd = math.gcd(gcd, A[i])
+
+if maxA >= K and K % gcd == 0:
     print('POSSIBLE')
 else:
     print('IMPOSSIBLE')
