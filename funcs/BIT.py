11145cd7e926fdfeaa883f222db76682b9eda789
Eguy <atyuge@gmail.com>
2021-10-06 03:42:43 +0900

    update

--- funcs/BIT.py
+++ funcs/BIT.py
@@ -6,6 +6,7 @@ class BIT:
   
     def sum(self, i):
         # i番目の要素までの総和を返す
+        # [1, i]
         s = 0
         while i > 0:
             s += self.tree[i]
@@ -13,6 +14,7 @@ class BIT:
         return s
 
     def range_sum(self, l, r):
+        # [l, r]の総和を返す
         if l == 1:
             return self.sum(r)
         else:
@@ -23,6 +25,21 @@ class BIT:
         while i <= self.size:
             self.tree[i] += x
             i += i & -i
+
+class RangeUpdate:
+    def __init__(self, n):
+        self.BIT0 = BIT(n + 1)
+        self.BIT1 = BIT(n + 1)
+
+    def add(self, l, r, x):
+        # [l, r]にxを加算
+        self.BIT0.add(l, -x*(l-1))
+        self.BIT0.add(r+1, x*r)
+        self.BIT1.add(l, x)
+        self.BIT1.add(r+1, -x)
+
+    def sum(self, i):
+        return self.BIT0.sum(i) + self.BIT1.sum(i) * i
 """
 # 配列Aの転倒数を求める
 N = int(input())
