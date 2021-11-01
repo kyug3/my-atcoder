cce0cb707a90be1ca248ee4463c9cd7358c920c7
Eguy <atyuge@gmail.com>
2021-11-02 01:13:00 +0900

    update

---
+++ ARC/ARC085/E.py
@@ -0,0 +1,80 @@
+import sys, math
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+mod = 10**9 + 7
+#mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
+from collections import deque
+
+class Dinic:
+    def __init__(self, n):
+        self.n = n
+        self.links = [[] for _ in range(n)]
+        self.depth = None
+        self.progress = None
+ 
+    def add_link(self, _from, to, cap):
+        self.links[_from].append([cap, to, len(self.links[to])])
+        self.links[to].append([0, _from, len(self.links[_from]) - 1])
+ 
+    def bfs(self, s):
+        depth = [-1] * self.n
+        depth[s] = 0
+        q = deque([s])
+        while q:
+            v = q.popleft()
+            for cap, to, rev in self.links[v]:
+                if cap > 0 and depth[to] < 0:
+                    depth[to] = depth[v] + 1
+                    q.append(to)
+        self.depth = depth
+ 
+    def dfs(self, v, t, flow):
+        if v == t:
+            return flow
+        links_v = self.links[v]
+        for i in range(self.progress[v], len(links_v)):
+            self.progress[v] = i
+            cap, to, rev = link = links_v[i]
+            if cap == 0 or self.depth[v] >= self.depth[to]:
+                continue
+            d = self.dfs(to, t, min(flow, cap))
+            if d == 0:
+                continue
+            link[0] -= d
+            self.links[to][rev][0] += d
+            return d
+        return 0
+ 
+    def max_flow(self, s, t):
+        flow = 0
+        while True:
+            self.bfs(s)
+            if self.depth[t] < 0:
+                return flow
+            self.progress = [0] * self.n
+            current_flow = self.dfs(s, t, INF)
+            while current_flow > 0:
+                flow += current_flow
+                current_flow = self.dfs(s, t, INF)
+
+N = int(input())
+A = li()
+d = Dinic(N+2)
+s = N
+t = s + 1
+tot = 0
+for i in range(N):
+    a = A[i]
+    if a < 0:
+        d.add_link(s, i, -a)
+    else:
+        tot += a
+        d.add_link(i, t, a)
+    i += 1
+    for j in range(i+i, N+1, i):
+        j -= 1
+        d.add_link(i-1, j, INF)
+
+print(tot - d.max_flow(s, t))
