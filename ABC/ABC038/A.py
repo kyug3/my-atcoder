ce0942b42773c9997447913341a9c24db1a041fa
Eguy <atyuge@gmail.com>
2021-11-27 20:46:37 +0900

    update

---
+++ ABC/ABC038/A.py
@@ -0,0 +1,13 @@
+import sys, math
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+mod = 10**9 + 7
+#mod = 998244353
+input = lambda: sys.stdin.readline().rstrip()
+def li(): return list(map(int, input().split()))
+
+S = input()
+if S[-1] == 'T':
+    print('YES')
+else:
+    print('NO')
