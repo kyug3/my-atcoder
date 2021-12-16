c75adf5c788d8321b972e0916b8fc5bb7607f955
Eguy <atyuge@gmail.com>
2021-12-17 04:27:19 +0900

    update

--- funcs/segtree/RMQ.py
+++ funcs/segtree/RMQ.py
@@ -1,50 +1,47 @@
 # N = 配列の長さ
-# data[N] が初期配列の 0 に対応する
+# self.tree[N] が初期配列の 0 に対応する
+class RMQ:
+    def __init__(self, N):
+        self.size = N
+        self.inf = 2**31-1
+        self.tree = [self.inf] * (2*N+1)
 
-#N = int(input())
-#data = [0] * (2*N+1)
-
-def query(l, r):
-    # 区間[l, r) の最小値を返す
-    L = l + N
-    R = r + N
-    ans = 2**31-1
-    while L < R:
-        if R % 2 == 1:
-            if ans > data[R-1]:
-                ans = data[R-1]
-            r -= 1
-        if L % 2 == 1:
-            if ans > data[L]:
-                ans = data[L]
-            L += 1
-        L //= 2; R //= 2
-    return ans
-
-def update(x, v):
-    # xをvに変更する
-    idx = x + N
-    data[idx] = v
-    while True:
-        idx //= 2
-        if idx == 0:
-            break
-        a, b = data[idx*2+1], data[idx*2]
-        if a > b:
-            data[idx] = b
-        else:
-            data[idx] = a
+    def query(self, l, r):
+        # return minimum element in range [l, r) 
+        L = l + self.size
+        R = r + self.size
+        ans = self.inf
+        while L < R:
+            if R % 2 == 1:
+                ans = min(ans, self.tree[R-1])
+                r -= 1
+            if L % 2 == 1:
+                ans = min(ans, self.tree[L])
+                L += 1
+            L //= 2; R //= 2
+        return ans
+    
+    def update(self, i, v):
+        # change A[i] to v
+        idx = i + self.size
+        self.tree[idx] = v
+        while True:
+            idx //= 2
+            if idx == 0:
+                break
+            a, b = self.tree[idx*2+1], self.tree[idx*2]
+            self.tree[idx] = min(a, b)
  
 
 # AOJ DSL_2_A
 # https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_A
 
 N, Q = map(int, input().split())
-data = [2**31-1] * (2*N+1)
+tree = RMQ(10**5 + 1)
 
 for _ in range(Q):
     q, x, y = list(map(int, input().split()))
     if q == 0:
-        update(x, y)
+        tree.update(x, y)
     else:
-        print(query(x, y+1))
+        print(tree.query(x, y+1))
