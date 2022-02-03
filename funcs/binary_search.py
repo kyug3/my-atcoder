db6e5d5da867b7b9118dd3a91fd45132e8803152
Eguy <atyuge@gmail.com>
2022-02-04 03:13:32 +0900

    update

--- funcs/binary_search.py
+++ funcs/binary_search.py
@@ -1,16 +1,15 @@
-def is_ok(idx, key):
-    # 条件を満たす最大のindexを求める
-    if lst[idx] > key:
-        return True
+def is_ok(x):
+    if x:
+        return 1
     else:
-        return False
+        return 0
 
-def binary_search(key):
+def binary_search():
     ng = -1
-    ok = len(lst)
-    while abs(ok - ng) > 1:
+    ok = 10**18 + 1
+    while ok - ng > 1:
         mid = (ok + ng) // 2
-        if is_ok(mid, key):
+        if is_ok(mid):
             ok = mid
         else:
             ng = mid
