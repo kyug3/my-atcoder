69e7e9df8ef92ee7a594604a4cb2f929f1bdccc2
Eguy <atyuge@gmail.com>
2022-04-09 17:42:40 +0900

    update

--- funcs/Rerooting(weighted).py
+++ funcs/Rerooting(weighted).py
@@ -1,23 +1,26 @@
 import sys, math
-#sys.setrecursionlimit(1000000)
-INF = 1 << 60
-mod = 1000000007
-#mod = 998244353
+sys.setrecursionlimit(1000000)
+INF = 1 << 100
+#mod = 1000000007
+mod = 998244353
 input = lambda: sys.stdin.readline().rstrip()
 li = lambda: list(map(int, input().split()))
+# https://atcoder.jp/contests/abc222/submissions/30708870
 
-N = li()
+# 重み付き木
+N = int(input())
 G = [[] for _ in range(N)]
 dic = {}
 idx = [0] * N
 for _ in range(N-1):
-    a, b = li()
+    a, b, c = li()
     a -= 1; b -= 1
-    G[a].append(b)
-    G[b].append(a)
-    dic[(a, b)] = idx[a]
-    dic[(b, a)] = idx[b]
+    G[a].append((c, b))
+    G[b].append((c, a))
+    dic[(a, b)] = (c, idx[a])
+    dic[(b, a)] = (c, idx[b])
     idx[a] += 1; idx[b] += 1
+D = li()
 
 from collections import deque
 
@@ -28,7 +31,7 @@ def toposort(G, v, N):
     while q:
         x = q.popleft()
         topo.append(x)
-        for y in G[x]:
+        for _, y in G[x]:
             if par[x] == y:
                 continue
             par[y] = x
@@ -36,7 +39,6 @@ def toposort(G, v, N):
     return topo, par
 topo, par = toposort(G, 0, N)
 dp = []
-dp2 = [0] * N
 for i in range(N):
     dp.append([0] * (idx[i]))
 
@@ -45,34 +47,33 @@ for i in range(N):
 for x in reversed(topo):
     y = par[x]
     if y != -1:
-        i = dic[(y, x)]
-        dp[y][i] = 0
-        dp2[y] = 0
+        c, i = dic[(y, x)]
+        if c + D[x] > max(dp[x]) + c:
+            dp[y][i] = c + D[x]
+        else:
+            dp[y][i] = max(dp[x]) + c
+
 
 for x in topo:
     l = len(dp[x])
     # 左からの累積何か
     leftmax = [0] * (l+2)
     for i in range(1, l+1):
-        leftmax[i] = leftmax[i-1] * dp[x][i-1]
+        leftmax[i] = max(leftmax[i-1], dp[x][i-1])
 
     # 右からの累積何か
     rightmax = [0] * (l+2)
     for i in range(1, l+1)[::-1]:
-        rightmax[i] = rightmax[i+1] * dp[x][i-1]
+        rightmax[i] = max(rightmax[i+1], dp[x][i-1])
     
     for i in range(l):
-        y = G[x][i]
-        if par[x] == y:
-            continue
-        j = dic[(y, x)]
+        c, y = G[x][i]
+        _, j = dic[(y, x)]
         #左右の累積を使って求めたい何かを取る
-        tmp = leftmax[i] * rightmax[i+2] + 1
+        tmp = max(leftmax[i], rightmax[i+2], D[x]) + c
 
-        #頂点yからその親への辺のdpを更新
-        dp[y][j] = 0
-        dp2[y] = 0
-
+        #頂点yからその親への辺についてのdpを更新
+        dp[y][j] = tmp
 
 for i in range(N):
-    print(dp2[i])
+    print(max(dp[i]))
