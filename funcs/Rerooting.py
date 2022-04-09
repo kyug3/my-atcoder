162d1ca896a219f3df1522585bfa954c1e313cc2
Eguy <atyuge@gmail.com>
2022-04-09 17:43:17 +0900

    update

--- funcs/Rerooting.py
+++ funcs/Rerooting.py
@@ -1,8 +1,8 @@
 import sys, math
-#sys.setrecursionlimit(1000000)
-INF = 1 << 60
-mod = 1000000007
-#mod = 998244353
+sys.setrecursionlimit(1000000)
+INF = 1 << 100
+#mod = 1000000007
+mod = 998244353
 input = lambda: sys.stdin.readline().rstrip()
 li = lambda: list(map(int, input().split()))
 
