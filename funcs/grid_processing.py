db8306a4dd1488d30761aa07311b1795bf201950
eguy <mazda.zoomzoom2009@gmail.com>
2022-08-20 21:45:55 +0900

    update

--- funcs/grid_processing.py
+++ funcs/grid_processing.py
@@ -10,3 +10,12 @@ def trim_extra(A, rem='.'):
         while all(x == rem for x in A[-1]):
             A.pop()
     return A
+
+# こっちのほうがはやい
+def rotate(X):
+    h = len(X); w = len(X[0])
+    NX = [[-1] * h for _ in range(w)]
+    for i in range(h):
+        for j in range(w):
+            NX[j][h-1-i] = X[i][j]
+    return NX
